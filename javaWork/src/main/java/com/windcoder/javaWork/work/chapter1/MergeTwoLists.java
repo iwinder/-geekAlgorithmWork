9aeb289dfd70b68841a58beed7dcfeacaf657de3
windcoder <windcoderz@foxmail.com>
2022-02-24 01:55:03 +0800

    feat(work): 第一周作业-合并链表
    合并链表，增加迭代和地柜方式

--- javaWork/src/main/java/com/windcoder/javaWork/work/chapter1/MergeTwoLists.java
+++ javaWork/src/main/java/com/windcoder/javaWork/work/chapter1/MergeTwoLists.java
@@ -9,6 +9,49 @@ import com.windcoder.javaWork.common.ListNode;
  */
 public class MergeTwoLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return list1;
+        // 创建新链表的头结点  curHead 以及用于执行链接两个链表的 初始节点 cur
+        ListNode curHead = new ListNode(0),cur = curHead;
+        // 其中一个链表为空即可结束循环
+        while(list1!=null&&list2!=null) {
+            // 当list1的值大于list2时，cur取list2，反之取list1
+            if(list1.val>=list2.val) {
+                cur.next = list2;
+                cur = cur.next;
+                list2 = list2.next;
+            } else {
+                cur.next = list1;
+                cur = cur.next;
+                list1 = list1.next;
+            }
+        }
+        // 其中一个为空后，将另一个列表的剩余部分直接追加即可，无需在进行多次循环。
+        cur.next = list1!=null? list1:list2;
+        return curHead.next;
     }
+
+    public ListNode mergeTwoListsByRecursion(ListNode list1, ListNode list2) {
+        ListNode curHead = new ListNode(0),cur = curHead;
+        recursion( list1,  list2, cur);
+        return curHead;
+    }
+
+    public ListNode recursion(ListNode list1, ListNode list2,ListNode cur) {
+        if(list1==null) {
+            cur.next = list2;
+            return cur;
+        } else if(list2==null) {
+            cur.next = list1;
+            return cur;
+        }if(list1.val>=list2.val) {
+            cur.next = list2;
+            cur = cur.next;
+            list2 = list2.next;
+        } else {
+            cur.next = list1;
+            cur = cur.next;
+            list1 = list1.next;
+        }
+        return recursion(list1,list2,cur);
+    }
+
 }
--- javaWork/src/main/java/com/windcoder/javaWork/work/chapter1/README.md
+++ javaWork/src/main/java/com/windcoder/javaWork/work/chapter1/README.md
@@ -1,6 +1,13 @@
 ## 第一周作业
 
 - [加一（Easy）](PlusOne.java)
+  - 倒序遍历数组，逐位+1。
+  - 加一后取模，不是0即可返回。
+  - 存在类似99的临界点，此时循环结束，数组中全是0，需要在最低位插入一位，其值为1
 - [合并两个有序链表（Easy）](MergeTwoLists.java)
+  - 创建新链表的头结点  curHead 以及用于执行链接两个链表的 初始节点 cur
+  - 循环结束条件：其中一个链表为空即可结束循环
+  - 当list1的值大于list2时，cur取list2，反之取list1
+  - 其中一个为空后，将另一个列表的剩余部分直接追加即可，无需在进行多次循环。
 - [设计循环双端队列（Medium）](MyCircularDeque.java)
 - [最大矩形（Hard）](MaximalRectangle.java)
