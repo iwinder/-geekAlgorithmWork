3d2b8c61ea908919fc6d8df84783a46ca1ad4b61
windcoder <windcoderz@foxmail.com>
2022-02-25 23:20:29 +0800

    feat(every): 每日一题5-有效括号

---
+++ javaWork/src/main/java/com/windcoder/javaWork/everyDay/day4/BraceIsValid.java
@@ -0,0 +1,35 @@
+package com.windcoder.javaWork.everyDay.day4;
+
+import java.util.Stack;
+
+/**
+ * 20. 有效的括号 https://leetcode-cn.com/problems/valid-parentheses/
+ */
+public class BraceIsValid {
+
+    public static boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<Character>();
+        for (char c:chars) {
+            if (c == '{' ||c=='['||c=='(') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                } else  if (c=='}' && stack.pop()!='{') {
+                    return false;
+                } else if (c==']' && stack.pop()!='[') {
+                    return false;
+                } else if (c==')' && stack.pop()!='(') {
+                    return false;
+                }
+            }
+
+        }
+        return stack.empty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("]"));
+    }
+}
