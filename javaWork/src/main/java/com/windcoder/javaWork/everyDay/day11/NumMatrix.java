5162d31c368aca6e2dcc2ff913998b60be95b3ee
windcoder <windcoderz@foxmail.com>
2022-03-06 20:06:15 +0800

    feat(work): 补充第二周作业-元素和为目标值的子矩阵数量
    增加 元素和为目标值的子矩阵数量 解法

---
+++ javaWork/src/main/java/com/windcoder/javaWork/everyDay/day11/NumMatrix.java
@@ -0,0 +1,24 @@
+package com.windcoder.javaWork.everyDay.day11;
+
+/**
+ * [304. 二维区域和检索 - 矩阵不可变](https://leetcode-cn.com/problems/range-sum-query-2d-immutable/)
+ */
+public class NumMatrix {
+    int n,m;
+    int sum[][];
+    public NumMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        sum=new int[n+1][m+1];
+        for (int i=1;i<=n;i++) {
+            for (int j=1;j<=m;j++) {
+                sum[i][j]=sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1]+matrix[i-1][j-1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        row1++;col1++;row2++;col2++;
+        return sum[row2][col2]-sum[row2][col1-1]-sum[row1-1][col2]+sum[row1-1][col1-1];
+    }
+}
--- javaWork/src/main/java/com/windcoder/javaWork/work/chapter2/NumSubmatrixSumTarget.java
+++ javaWork/src/main/java/com/windcoder/javaWork/work/chapter2/NumSubmatrixSumTarget.java
@@ -1,7 +1,63 @@
 package com.windcoder.javaWork.work.chapter2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * [1074. 元素和为目标值的子矩阵数量](https://leetcode-cn.com/problems/number-of-submatrices-that-sum-to-target/)
  */
 public class NumSubmatrixSumTarget {
+    int n,m;
+
+
+    /**
+     * 计算元素和为目标值的子矩阵数量
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int sum[];
+        int count = 0;
+        for (int i=0;i<n;i++) { // 上限
+            sum = new int[m];
+            for (int j=i;j<n;j++) { // 下限，由于是行的下限，所以 i<= j<n
+                for (int c=0;c<m;c++) { // 列
+                    sum[c] +=matrix[j][c]; // 记录到j行时，每一c列的和，从而转换成前n列的
+                }
+                //
+                count +=subarraySum(sum,target); // 转换为计算子项前缀和符合的次数
+            }
+        }
+        return count;
+    }
+
+    public int subarraySum(int[] nums, int k) {
+        int pre=0,len =nums.length,count=0;
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for (int i=0;i<len;i++) {
+            pre+=nums[i];
+            if (map.containsKey(pre-k)) {
+                count += map.get(pre-k);
+            }
+            map.put(pre,map.getOrDefault(pre,0)+1);
+        }
+        return count;
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        int[][] anum = {{1,-1},{-1,1}};
+        NumSubmatrixSumTarget target = new NumSubmatrixSumTarget();
+        System.out.println(target.numSubmatrixSumTarget(anum,0));
+    }
+
 }
