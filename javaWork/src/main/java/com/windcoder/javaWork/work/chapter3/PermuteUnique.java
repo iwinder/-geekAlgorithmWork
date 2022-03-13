49126e18a4279cd7d5d2b6cdecc52fc6667a3742
windcoder <windcoderz@foxmail.com>
2022-03-14 00:28:27 +0800

    feat(work): 补充第三次作业

--- javaWork/src/main/java/com/windcoder/javaWork/work/chapter3/PermuteUnique.java
+++ javaWork/src/main/java/com/windcoder/javaWork/work/chapter3/PermuteUnique.java
@@ -4,13 +4,45 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * [47. 全排列 II](https://leetcode-cn.com/problems/permutations-ii/)
+ */
 public class PermuteUnique {
     boolean[] vis;
 
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> perm = new ArrayList<>();
+        // 初始化数组，记录第一次出现的位置
         vis = new boolean[nums.length];
+        // 对原数组排序，保证相同的数字相邻
         Arrays.sort(nums);
+        backtrack(nums,result,0,perm);
+        return result;
+    }
+
+    /**
+     * 填充 perm,idx
+     * @param nums
+     * @param result 最终结果集
+     * @param idx 填充位置，当为n说明已经填充完成一个数组，所以result增加一个并返回
+     * @param perm 每次的当前排列
+     */
+    public void backtrack(int[] nums, List<List<Integer>> result,int idx,List<Integer> perm) {
+        int len = nums.length;
+        if (idx == len) {
+            result.add(new ArrayList<Integer>(perm));
+            return;
+        }
+        for (int i=0;i<len;i++) {
+            if (vis[i] || (i>0&&nums[i]==nums[i-1]&&!vis[i-1])) {
+                continue;
+            }
+            perm.add(nums[i]);
+            vis[i] = true;
+            backtrack(nums,result,idx+1,perm);
+            vis[i] = false;
+            perm.remove(idx);
+        }
     }
 }
--- javaWork/src/main/java/com/windcoder/javaWork/work/chapter3/README.md
+++ javaWork/src/main/java/com/windcoder/javaWork/work/chapter3/README.md
@@ -1,7 +1,8 @@
 ## 第二周作业
 
 - [合并K个升序链表（Hard））-- MergeKLists.java](MergeKLists.java)
-- [数组的度（Easy）-- FindShortestSubArray.java ](FindShortestSubArray.java)
-- [和为 K 的子数组（Medium）-- SubarraySum.java ](SubarraySum.java)
-- [元素和为目标值的子矩阵数量（Hard）-- NumSubmatrixSumTarget.java ](NumSubmatrixSumTarget.java) 
+- [全排列 II（Easy）-- PermuteUnique.java ](PermuteUnique.java)
+- [从中序与后序遍历序列构造二叉树（Medium）-- 待完成 ]()
+- [课程表 II （Medium）-- 待完成 ]() 
+- [冗余连接 II （Hard）-- 待完成 ]() 
    
